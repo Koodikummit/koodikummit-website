022fdfb3bec9eabf93d53f083346530f39848016
Niko Salminen <niko@salminen.me>
2023-02-02 21:06:11 +0200

    Add grid to company page

--- src/sections/ForCompanies.js
+++ src/sections/ForCompanies.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import { Grid } from '@mui/material';
+import styled from 'styled-components';
+import Title from '../components/Title';
+import HeroCompanies from '../assets/hero-companies.svg';
+
+const SvgContainer = styled.div`
+height: 470px;
+width: 470px;
+@media (max-width: 768px) {
+  height: 220px;
+  width: 220px;
+}
+`
 
 const ForCompanies = () => (
-  <div id="for-companies"></div>
+  <section id="for-companies">
+    <Grid container spacing={2}>
+      <Title>How does this affect our company?</Title>
+      <Grid item sm={6}>
+        Lorem ipsum
+      </Grid>
+      <Grid item sm={6}>
+        <SvgContainer>
+          <HeroCompanies height="100%" width="100%" />
+        </SvgContainer>
+      </Grid>
+    </Grid>
+  </section>
 )
 
 export default ForCompanies;
