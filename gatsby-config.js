52149a4c7e2a3868182ae442f9496101943a7dff
Joni Rämö <joni.ramo@vincit.fi>
2023-03-09 22:11:32 +0200

    Adjust hero title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,38 @@
-require("dotenv").config({
+require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
-})
+});
 
 module.exports = {
   siteMetadata: {
     title: `Koodikummit`,
-    siteUrl: `https://koodikummit.org`
+    siteUrl: `https://koodikummit.org`,
   },
   plugins: [
-    "gatsby-plugin-styled-components",
+    'gatsby-plugin-styled-components',
     {
-      resolve: "gatsby-plugin-react-svg",
+      resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
-          include: /assets/
-        }
-      }
+          include: /assets/,
+        },
+      },
     },
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [
-          `exo`,
-          `space grotesk`
-        ],
-        display: 'swap'
-      }
+        fonts: [`exo`, `space grotesk`, 'krona one'],
+        display: 'swap',
+      },
     },
     {
-      resolve: "gatsby-source-contentful",
+      resolve: 'gatsby-source-contentful',
       options: {
         accessToken: process.env.ACCESS_TOKEN,
         spaceId: process.env.SPACE_ID,
-        host: process.env.CONTENTFUL_HOST || 'cdn.contentful.com'
+        host: process.env.CONTENTFUL_HOST || 'cdn.contentful.com',
       },
     },
 
-    "gatsby-plugin-image",
-
-  ]
+    'gatsby-plugin-image',
+  ],
 };
--- src/sections/Main.js
+++ src/sections/Main.js
@@ -1,12 +1,32 @@
 import React from 'react';
 
-import Title from '../components/common/Title';
 import SectionContainer from '../components/common/SectionContainer';
 import Hero from '../assets/hero-main.svg';
+import styled from 'styled-components';
+
+const Subtitle = styled.h1`
+  font-family: 'Krona One', sans-serif;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 30px;
+  line-height: 30px;
+  margin: 50px 20px;
+  text-align: center;
+`;
+
+const HeroTitle = styled.h1`
+  font-family: 'Krona One', sans-serif;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 125px;
+  line-height: 125px;
+  margin: 20px;
+`;
 
 const Main = () => (
   <SectionContainer id="main">
-    <Title>Koodikummit</Title>
+    <Subtitle>Welcome to</Subtitle>
+    <HeroTitle>Koodikummit</HeroTitle>
     <Hero />
   </SectionContainer>
 );
